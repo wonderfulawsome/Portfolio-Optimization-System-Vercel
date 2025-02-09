f313a8bf6d689193ec30e94295e4a723fc5d8999
Park Sungsoo <150274144+wonderfulawsome@users.noreply.github.com>
2025-02-09 14:15:44 +0900

    Add files via upload

---
+++ api.js
@@ -0,0 +1,18 @@
+// api.js
+export async function optimizePortfolio(data) {
+    try {
+      const response = await fetch("https://portfolio-optimization-system-flask.onrender.com/optimize", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return await response.json();
+    } catch (error) {
+      console.error("Error fetching portfolio:", error);
+      return null;
+    }
+  }
+  
